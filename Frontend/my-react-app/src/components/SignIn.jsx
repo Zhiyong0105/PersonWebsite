db6a97a46e8eb284433e24308fbd5aec6a9a4555
Zhiyong0105 <80504168+Zhiyong0105@users.noreply.github.com>
2024-12-12 23:02:57 +0900

    Back up

--- Frontend/my-react-app/src/components/SignIn.jsx
+++ Frontend/my-react-app/src/components/SignIn.jsx
@@ -52,17 +52,33 @@ export default function SignIn ({ toggleToRegister,setAlert,toggleToSwithUserBut
     }
   };
   return (
-    <form className="w-4/5" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="用户名"
-        className="w-full bg-gray-200 p-3 rounded-lg mb-4 focus:outline-none focus:ring-2 focus:ring-blue-400 transition duration-300"
-      />
-      <input
-        type="password"
-        placeholder="密码"
-        className="w-full bg-gray-200 p-3 rounded-lg mb-4 focus:outline-none focus:ring-2 focus:ring-blue-400 transition duration-300"
-      />
+    <form className="w-4/5 flex flex-col gap-4" onSubmit={handleSubmit}>
+ 
+<label className="input input-bordered flex items-center gap-4">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 16 16"
+    fill="currentColor"
+    className="h-4 w-4 opacity-70">
+    <path
+      d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+  </svg>
+  <input type="text" className="grow" placeholder="Username" />
+</label>
+
+<label className="input input-bordered flex items-center gap-4">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 16 16"
+    fill="currentColor"
+    className="h-4 w-4 opacity-70">
+    <path
+      fillRule="evenodd"
+      d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+      clipRule="evenodd" />
+  </svg>
+  <input type="password" className="grow"  placeholder="******" />
+</label>
       <button
         type="submit"
         className="w-full px-4 py-2 bg-blue-500 text-white rounded-lg shadow hover:bg-blue-600 transition duration-300"
